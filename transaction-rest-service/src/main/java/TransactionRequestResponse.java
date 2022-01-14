5517e58b022d497360e8e909da592bf340a7333a
gustav <kirkholtgustav@gmail.com>
2022-01-14 11:27:00 +0100

    Transaction request response

---
+++ transaction-rest-service/src/main/java/TransactionRequestResponse.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionRequestResponse {
+    private boolean successful;
+    private String errorMessage;
+
+    public TransactionRequestResponse(boolean s) {
+        this.successful = s;
+    }
+}
--- transaction-rest-service/src/main/java/TransactionResource.java
+++ transaction-rest-service/src/main/java/TransactionResource.java
@@ -1,21 +1,38 @@
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import messaging.Event;
 import messaging.MessageQueue;
 
+import java.util.concurrent.CompletableFuture;
+
 @Path("/transactions")
 public class TransactionResource {
 
-    MessageQueue queue;
+    private MessageQueue queue;
+    private CompletableFuture<TransactionRequestResponse> response;
+
+    public TransactionResource () {
+        this.queue.addHandler("TransactionRequestResponse", this::handleTransactionRequestResponse);
+
+    }
+
+    private void handleTransactionRequestResponse(Event event) {
+        TransactionRequestResponse r = event.getArgument(0, TransactionRequestResponse.class);
+        response.complete(r);
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void createTransaction(TransactionRestRequest request) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public TransactionRequestResponse createTransaction(TransactionRestRequest request) {
         var event = new Event("TransactionRequest", new Object[] {request});
         this.queue.publish(event);
+        response = new CompletableFuture<>();
+        return response.join();
 
     }
 
