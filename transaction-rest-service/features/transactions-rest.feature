baab4dcf5f954d2849df5cf7449ce18c0573c7ba
gustav <kirkholtgustav@gmail.com>
2022-01-14 15:17:30 +0100

    Transaction rest test steps

--- transaction-rest-service/features/transactions-rest.feature
+++ transaction-rest-service/features/transactions-rest.feature
@@ -1,2 +1,6 @@
 Feature: Transaction Rest Service
-  Scenario:
+  Scenario: sending a transaction request
+    When a "TransactionRestRequest" event is received
+    Then a "TransactionRestRequestResponse" event is sent
+    And the transaction response has status successful
+
--- transaction-rest-service/src/test/java/TransactionRestServiceTest.java
+++ transaction-rest-service/src/test/java/TransactionRestServiceTest.java
@@ -1,7 +1,13 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
+import messaging.MessageQueue;
 import org.junit.runner.RunWith;
+
+import static org.mockito.Mockito.mock;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin="summary"
@@ -11,5 +17,25 @@ import org.junit.runner.RunWith;
 )
 
 public class TransactionRestServiceTest {
+
+    MessageQueue queue = mock(MessageQueue.class);
+
+
+    @When("a {string} event is received")
+    public void a_event_is_received(String string) {
+
+    }
+
+    @Then("a {string} event is sent")
+    public void a_event_is_sent(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("the transaction response has status successful")
+    public void the_transaction_response_has_status_successful() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
