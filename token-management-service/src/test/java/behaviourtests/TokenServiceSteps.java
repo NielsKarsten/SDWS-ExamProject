07640b31cb50e8b3c70d35eea7850e5e45218c71
Christian <christian.gernsoe@gmail.com>
2022-01-18 18:59:53 +0100

    Test

--- token-management-service/src/test/java/behaviourtests/TokenServiceSteps.java
+++ token-management-service/src/test/java/behaviourtests/TokenServiceSteps.java
@@ -64,6 +64,7 @@ public class TokenServiceSteps {
 		exception = assertThrows(IllegalArgumentException.class, () -> {
 			tokenService.requestTokens(tokenRequest);
 		});
+		//Niels vil teste, men ikke teste en test
 	}
 	
 	public Object createEventObject(String eventName) {
