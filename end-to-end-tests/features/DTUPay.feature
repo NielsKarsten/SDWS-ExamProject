48a20c71ce36c95fc933e3d0bdc4d0d47c1df95f
Christian <christian.gernsoe@gmail.com>
2022-01-18 16:29:06 +0100

    Added before/after steps and changed some tests

--- end-to-end-tests/features/DTUPay.feature
+++ end-to-end-tests/features/DTUPay.feature
@@ -7,87 +7,68 @@
 Feature: DTU Pay feature
 
 Scenario: Successful payment
-	Given a customer "Johnny" "Bravo" with bank account "1337"
+	Given a customer "Johnny" "Bravo"
 	When customer is being registered
-	Given a merchant "Bravo" "Johnny" with bank account "1111"
+	Given a merchant "Bravo" "Johnny"
 	When merchant is being registered
 	When customer requests 5 tokens
 	Then customer has 5 tokens
 	When merchant initiates a transaction for 100
 	Then customer has balance 0
 	And merchant has balance 100
-	When account with id "1337" is retired
-	And account with id "1111" is retired
-	Then account with id "1337" does not exist
-	And account with id "1337" does not exist
+	When customer account is retired
+	Then account does not exist
+	When merchant account is retired
+	Then account does not exist
 
 Scenario: Customer gets transaction
-	Given a customer "Johnny" "Bravo" with bank account "1337"
+	Given a customer "Johnny" "Bravo"
 	When customer is being registered
-	Given a merchant "Bravo" "Johnny" with bank account "1111"
+	Given a merchant "Bravo" "Johnny"
 	When merchant is being registered
 	When customer requests 5 tokens
 	Then customer has 5 tokens
 	When merchant initiates a transaction for 100
 	And customer requests transactions
 	Then user gets transaction
-	When account with id "1337" is retired
-	And account with id "1111" is retired
-	Then account with id "1337" does not exist
-	And account with id "1337" does not exist
+	When customer account is retired
+	Then account does not exist
+	When merchant account is retired
+	Then account does not exist
 
 
 Scenario: Merchant gets transaction
-	Given a customer "Johnny" "Bravo" with bank account "1337"
+	Given a customer "Johnny" "Bravo"
 	When customer is being registered
-	Given a merchant "Bravo" "Johnny" with bank account "1111"
+	Given a merchant "Bravo" "Johnny"
 	When merchant is being registered
 	When customer requests 5 tokens
 	Then customer has 5 tokens
 	When merchant initiates a transaction for 100
 	And merchant requests transactions
 	Then user gets transaction
-	When account with id "1337" is retired
-	And account with id "1111" is retired
-	Then account with id "1337" does not exist
-	And account with id "1337" does not exist
+	When customer account is retired
+	Then account does not exist
+	When merchant account is retired
+	Then account does not exist
 
 Scenario: Admin gets transaction
-	Given a customer "Johnny" "Bravo" with bank account "1337"
+	Given a customer "Johnny" "Bravo"
 	When customer is being registered
-	Given a merchant "Bravo" "Johnny" with bank account "1111"
+	Given a merchant "Bravo" "Johnny"
 	When merchant is being registered
 	When customer requests 5 tokens
 	Then customer has 5 tokens
 	When merchant initiates a transaction for 100
 	And admin requests transactions
 	Then user gets transaction
-	When account with id "1337" is retired
-	And account with id "1111" is retired
-	Then account with id "1337" does not exist
-	And account with id "1337" does not exist
+	When customer account is retired
+	Then account does not exist
+	When merchant account is retired
+	Then account does not exist
 
 Scenario: Admin creates and deletes
-	Given an admin "Johnny" "Bravo" with bank account "1337"
+	Given an admin "Johnny" "Bravo"
 	When admin is being registered
-	And account with id "1337" is retired
-	Then account with id "1337" does not exist
-
-#customerAccount = bank.createAccount Name, CPR, balance
-#merchantAccount = bank.createAccount Name, CPR, balance
-#cid = customerAPI.register Name, customerAccount, ...
-#this will register the customer via the customer port of DTU Pay
-#mid = merchantAPI.register Name, merchantAccount, ...
-#this will register the merchant via the merchant port of DTU Pay
-#tokens = customerAPI.getTokens 5, cid
-#this will get 5 tokens via the customer port of DTU Pay
-#token = select a token from tokens
-#merchantAPI.pay token, mid, amount
-#this will execute the payment via the merchant port of DTU Pay
-#and in DTU Pay, this will cause a call the money transfer service
-#of the bank
-#customerBalance = bank.getBalance customer account
-#merchantBalance = bank.getBalance merchant account
-#check that customerBalance and merchantBlance are correct
-#bank.retire customer account
-#bank.retire merchant account
+	When admin account is retired
+	Then account does not exist
--- end-to-end-tests/src/test/java/behaviourtests/DTUPaySteps.java
+++ end-to-end-tests/src/test/java/behaviourtests/DTUPaySteps.java
@@ -9,9 +9,12 @@ import static org.junit.Assert.assertEquals;
 
 import dtu.ws.fastmoney.test.BankService;
 import dtu.ws.fastmoney.test.BankServiceException_Exception;
+import dtu.ws.fastmoney.test.User;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.After;
+import org.junit.Before;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -21,54 +24,77 @@ import javax.ws.rs.core.GenericType;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class DTUPaySteps {
 	private BankService bankService = mock(BankService.class);;
-	private User customer;
-	private User merchant;
-	private User admin;
+	private DTUPayUser customer;
+	private DTUPayUser merchant;
+	private DTUPayUser admin;
 	private List<UUID> tokens;
 	private UUID transactionToken;
 	private BigDecimal transactionAmount;
 	private List<Transaction> transactionList;
+	private User johnnyUser;
+	private User bravoUser;
+	private String customerAccountId;
+	private String merchantAccountId;
+	private boolean deleteAccountResponse;
 
 	Client client = ClientBuilder.newClient();
 	WebTarget merchantTarget = client.target("http://localhost:8080/").path("merchant");
 	WebTarget customerTarget = client.target("http://localhost:8080/").path("customer");
 	WebTarget adminTarget = client.target("http://localhost:8080/").path("admin");
 
-	@Given("a customer {string} {string} with bank account {string}")
-	public void aCustomerWithBankAccount(String firstName, String lastName, String accountId) {
-		customer = new User(firstName, lastName, accountId);
+	@Before
+	public void createBankAccount() throws BankServiceException_Exception {
+		johnnyUser = new User();
+		johnnyUser.setCprNumber("12341234");
+		johnnyUser.setFirstName("Johnny");
+		johnnyUser.setLastName("Bravo");
+
+		bravoUser = new User();
+		bravoUser.setCprNumber("12353253426");
+		bravoUser.setFirstName("Bravo");
+		bravoUser.setLastName("Johnny");
+
+		customerAccountId = bankService.createAccountWithBalance(johnnyUser, BigDecimal.valueOf(1000));
+		merchantAccountId = bankService.createAccountWithBalance(bravoUser, BigDecimal.valueOf(1000));
+	}
+
+	@Given("a customer {string} {string}")
+	public void aCustomerWithBankAccount(String firstName, String lastName) {
+		customer = new DTUPayUser(firstName, lastName, customerAccountId);
 		customer.assignUserId();
 	}
 
-	@Given("a merchant {string} {string} with bank account {string}")
-	public void aMerchantWithBankAccount(String firstName, String lastName, String accountId) {
-		merchant = new User(firstName, lastName, accountId);
+	@Given("a merchant {string} {string}")
+	public void aMerchantWithBankAccount(String firstName, String lastName) {
+		merchant = new DTUPayUser(firstName, lastName, merchantAccountId);
 		merchant.assignUserId();
 	}
 
-	@Given("an admin {string} {string} with bank account {string}")
-	public void anAdminWithBankAccount(String firstName, String lastName, String accountId) {
-		admin = new User(firstName, lastName, accountId);
+	@Given("an admin {string} {string}")
+	public void anAdminWithBankAccount(String firstName, String lastName) {
+		admin = new DTUPayUser(firstName, lastName, customerAccountId);
 		admin.assignUserId();
 	}
 
 	@When("customer is being registered")
 	public void theCustomerIsBeingRegistered() {
-		customerTarget.request().post(Entity.json(customer), User.class);
+		customerTarget.request().post(Entity.json(customer), DTUPayUser.class);
 	}
 
 	@When("merchant is being registered")
 	public void theMerchantIsBeingRegistered() {
-		merchantTarget.request().post(Entity.json(merchant), User.class);
+		merchantTarget.request().post(Entity.json(merchant), DTUPayUser.class);
 	}
 
 	@When("admin is being registered")
 	public void theAdminIsBeingRegistered() {
-		adminTarget.request().post(Entity.json(admin), User.class);
+		adminTarget.request().post(Entity.json(admin), DTUPayUser.class);
 	}
 
 	@When ("customer requests {int} tokens")
@@ -119,23 +145,42 @@ public class DTUPaySteps {
 	@Then("customer has balance {int}")
 	public void theCustomerHasBalance(int amount) throws BankServiceException_Exception {
 		BigDecimal actual = bankService.getAccount(customer.getAccountId()).getBalance();
-		assertEquals(actual, amount);
+		assertEquals(actual, BigDecimal.valueOf(amount));
 	}
 
 	@Then("merchant has balance {int}")
 	public void theMerchantHasBalance(int amount) throws BankServiceException_Exception {
 		BigDecimal actual = bankService.getAccount(merchant.getAccountId()).getBalance();
-		assertEquals(actual, amount);
+		assertEquals(actual, BigDecimal.valueOf(amount));
 	}
 
-	@When("account with id {string} is retired")
-	public void theAccountIsRetired(String accountId) throws BankServiceException_Exception {
-		bankService.retireAccount(accountId);
+	@When("customer account is retired")
+	public void theCustomerAccountIsDeleted() {
+		var response = customerTarget.queryParam("userId", customer.getUserId()).request().delete();
+		deleteAccountResponse = response.readEntity(boolean.class);
 	}
 
-	@Then("account with id {string} does not exist")
-	public void theAccountDoesNotExist(String accountId) throws BankServiceException_Exception {
-		assertEquals("null", bankService.getAccount(accountId));
+	@When("merchant account is retired")
+	public void theMerchantAccountIsDeleted() {
+		var response = merchantTarget.queryParam("userId", merchant.getUserId()).request().delete();
+		deleteAccountResponse = response.readEntity(boolean.class);
+	}
+
+	@When("admin account is retired")
+	public void theAdminAccountIsDeleted() {
+		var response = adminTarget.queryParam("userId", admin.getUserId()).request().delete();
+		deleteAccountResponse = response.readEntity(boolean.class);
+	}
+
+	@Then("account does not exist")
+	public void theAccountDoesNotExist() {
+		assertTrue(deleteAccountResponse);
+	}
+
+	@After
+	public void retireBankAccount() throws BankServiceException_Exception {
+		bankService.retireAccount(customerAccountId);
+		bankService.retireAccount(merchantAccountId);
 	}
 }
 
