786cd9adaead2823032e0e8e5ed2b81ae96e3e44
Niels Bisgaard-Bohr <nkbdeltora@hotmail.com>
2022-01-18 11:43:54 +0100

    Create TokenRequest.java
    Co-Authored-By: Johnny322 <19227408+Johnny322@users.noreply.github.com>
    Co-Authored-By: Gustav Utke Kauman <13206187+gustavkauman@users.noreply.github.com>

---
+++ rest-api/src/main/java/models/TokenRequest.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.io.Serializable;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequest implements Serializable {
+    private UUID userId;
+    private int amount;
+}
