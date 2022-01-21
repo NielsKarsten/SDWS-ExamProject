8400321d2d8e405bb71b114d67fd083ea49f1e60
Niels Bisgaard-Bohr <nkbdeltora@hotmail.com>
2022-01-21 16:38:22 +0100

    Updated Transaction-service
    Co-Authored-By: GustavKirkholt <55699615+GustavKirkholt@users.noreply.github.com>
    Co-Authored-By: gernsoe <43669576+gernsoe@users.noreply.github.com>
    Co-Authored-By: Gustav Utke Kauman <13206187+gustavkauman@users.noreply.github.com>
    Co-Authored-By: theogutt <20193902+theogutt@users.noreply.github.com>
    Co-Authored-By: Thomas Strange <21693787+TRStrange@users.noreply.github.com>
    Co-Authored-By: Johnny322 <19227408+Johnny322@users.noreply.github.com>

--- transaction-service/src/main/java/transaction/service/connector/AccountServiceConnector.java
+++ transaction-service/src/main/java/transaction/service/connector/AccountServiceConnector.java
@@ -1,59 +1,31 @@
 package transaction.service.connector;
 
-import messaging.Event;
 import messaging.MessageQueue;
-import transaction.service.models.User;
-import transaction.service.services.EventType;
 
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
+import handling.AccountEventType;
+import handling.GenericHandler;
+import handling.TokenEventType;
+import handling.TransactionEventType;
 
-public class AccountServiceConnector {
-
-    MessageQueue queue;
-
-    private Map<UUID, CompletableFuture<Object>> correlations = new ConcurrentHashMap<>();
+public class AccountServiceConnector extends GenericHandler implements  AccountEventType, TokenEventType, TransactionEventType{
 
     public AccountServiceConnector(MessageQueue q) {
-        this.queue = q;
-        this.queue.addHandler("UserAccountInfoResponse", this::handleGetAccountFromIdResponse);
-        this.queue.addHandler("VerifyUserAccountExistsResponse", this::handleVerifyUserAccountExistsResponse);
-        this.queue.addHandler("UserAccountInvalid", this::handleVerifyUserAccountExistsResponse);
+    	super(q);
+        addHandler("UserAccountInfoResponse", this::genericHandler);
+        addHandler("VerifyUserAccountExistsResponse", this::genericHandler);
+        addHandler("UserAccountInvalid", this::genericHandler);
     }
 
     public boolean userExists(UUID userId) throws NullPointerException {
         if (userId == null)
             throw new NullPointerException("User Id was not specified");
-
-        UUID correlationId = UUID.randomUUID();
-        correlations.put(correlationId, new CompletableFuture<>());
-        Event e = new Event(correlationId, EventType.VERIFY_USER_ACCOUNT_EXISTS_REQUEST, new Object[] { userId });
-        this.queue.publish(e);
-        return (boolean) correlations.get(correlationId).join();
-    }
-
-    public void handleVerifyUserAccountExistsResponse(Event event) {
-        UUID correlationId = event.getCorrelationId();
-        boolean success = event.getArgument(0, boolean.class);
-        correlations.get(correlationId).complete(success);
+        return (boolean) buildCompletableFutureEvent(userId, VERIFY_USER_ACCOUNT_EXISTS_REQUESTS);
     }
 
     public String getUserBankAccountFromId(UUID userId) throws NullPointerException {
         if (userId == null)
             throw new NullPointerException("User Id was not specified");
-
-        UUID correlationId = UUID.randomUUID();
-        correlations.put(correlationId, new CompletableFuture<>());
-        Event e = new Event(correlationId, EventType.USER_ACCOUNT_INFO_REQUESTED, new Object[] { userId });
-        this.queue.publish(e);
-        return (String) correlations.get(correlationId).join();
-    }
-
-    public void handleGetAccountFromIdResponse(Event event) {
-        String userBankAccountId = event.getArgument(0, String.class);
-        UUID correlationId = event.getCorrelationId();
-        correlations.get(correlationId).complete(userBankAccountId);
+        return (String) buildCompletableFutureEvent(userId, USER_ACCOUNT_INFO_REQUESTED);
     }
 }
--- transaction-service/src/main/java/transaction/service/connector/TokenServiceConnector.java
+++ transaction-service/src/main/java/transaction/service/connector/TokenServiceConnector.java
@@ -1,50 +1,23 @@
 package transaction.service.connector;
 
-import messaging.Event;
 import messaging.MessageQueue;
-import transaction.service.models.User;
-import transaction.service.services.EventType;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
+import handling.GenericHandler;
+import handling.TokenEventType;
 
 /**
  * Connector class to handle communication with the token service
  * Author: Gustav Utke Kauman (s195396), Gustav Lintrup Kirkholt, Niels
  * Bisgaard-Bohr (S202745)
  */
-public class TokenServiceConnector {
-
-    private MessageQueue queue;
-    private Map<UUID, CompletableFuture<UUID>> correlations = new ConcurrentHashMap<>();
-
+public class TokenServiceConnector extends GenericHandler implements  TokenEventType{
     public TokenServiceConnector(MessageQueue q) {
-        this.queue = q;
-        this.queue.addHandler("TokenToCustomerIdResponse", this::handleGetUserFromTokenResponse);
-        this.queue.addHandler("TokenToCustomerIdResponseInvalid", this::handleGetUserIdFromTokenError);
+    	super(q);
+        addHandler(TOKEN_TO_CUSTOMER_ID_RESPONSE, this::genericHandler);
+        addHandler(TOKEN_TO_CUSTOMER_ID_RESPONSE_INVALID, this::genericErrorHandler);
     }
 
     public UUID getUserIdFromToken(UUID token) throws Exception {
-        UUID correlationId = UUID.randomUUID();
-        correlations.put(correlationId, new CompletableFuture<>());
-        Event event = new Event(correlationId, EventType.TOKEN_TO_CUSTOMER_ID_REQUESTED, new Object[] { token });
-        queue.publish(event);
-        return correlations.get(correlationId).join();
-    }
-
-    public void handleGetUserIdFromTokenError(Event e) {
-        UUID correlationId = e.getCorrelationId();
-        String errorMessage = e.getArgument(0, String.class);
-        IllegalArgumentException exception = new IllegalArgumentException(errorMessage);
-        correlations.get(correlationId).completeExceptionally(exception);
-    }
-
-    public void handleGetUserFromTokenResponse(Event e) {
-        UUID userId = e.getArgument(0, UUID.class);
-        UUID correlationId = e.getCorrelationId();
-        correlations.get(correlationId).complete(userId);
+        return UUID.fromString((String) buildCompletableFutureEvent(token, TOKEN_TO_CUSTOMER_ID_REQUESTED));
     }
 }
